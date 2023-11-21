25e79f2d81dedd44baac622c81ce3ee85b84c96d
Junaid Syed <sjunaid626@gmail.com>
2023-11-21 19:42:24 +0530

    Generate schema.rb

---
+++ db/schema.rb
@@ -0,0 +1,22 @@
+ActiveRecord::Schema[7.0].define(version: 2023_11_21_135836) do
+  enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "products", "users"
+end
