3fbe998bc7abacfe0d7a2c544ad0bb45ef122630
Junaid Syed <sjunaid626@gmail.com>
2024-01-06 20:31:53 +0530

    Create orders_controller.rb

---
+++ app/controllers/api/v1/orders_controller.rb
@@ -0,0 +1,39 @@
+  
+  def index
+    @orders = Order.all
+    render json: @orders
+  end
+
+  def show
+    find_order
+    render json: @order
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      render json: @order, status: :created
+    else
+      render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    find_order
+    if @order.update(order_params)
+      render json: @order
+    else
+      render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    find_order
+    @order.destroy
+  end
+
+  private
+
+  def find_order
+    @order = Order.find(params[:id])
+  end
