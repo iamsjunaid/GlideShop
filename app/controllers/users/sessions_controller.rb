ebef9f556c013b4b5a44ca07a3aba1d08fa85e3a
Junaid Syed <sjunaid626@gmail.com>
2024-01-13 12:19:26 +0530

    Update sessions_controller.rb

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,10 +5,16 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(current_user, _opts = {})
-    render json: {
-      status: { code: 200, message: 'Logged in sucessfully.' },
-      data: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes] }
-    }, status: :ok
+    if current_user.present?
+      render json: {
+        status: { code: 200, message: 'Logged in successfully.' },
+        data: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes] }
+      }, status: status_code
+    else
+      render json: {
+        status: { code: 401, message: 'Unauthorized. Please check your credentials.' }
+      }, status: :unauthorized
+    end
   end
 
   def respond_to_on_destroy
